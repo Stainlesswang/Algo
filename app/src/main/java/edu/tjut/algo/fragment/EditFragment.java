0a0a0aa423946a19b25eb9fe9a66d092b7ff7ea4
thinkpad <gpsok@sina.cn>
2018-04-18 19:12:19 +0800

    4.18

--- app/src/main/java/edu/tjut/algo/fragment/EditFragment.java
+++ app/src/main/java/edu/tjut/algo/fragment/EditFragment.java
@@ -8,9 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import edu.tjut.algo.R;
+import edu.tjut.algo.data.TestData;
+
 public class EditFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -22,7 +29,14 @@ public class EditFragment extends Fragment {
     private EditText value=null;
     private Button btn_add=null;
     private Button btn_finshAdd=null;
+    private Button btn_inputDo=null;
     private ListView input_list=null;
+    private Spinner sp_input_selectMethod=null;
+    private EditText input_capacity=null;
+    private ArrayList<Integer> weights=new ArrayList<>();
+    private ArrayList<Integer> values=new ArrayList<>();
+    private LinearLayout linearLayout_inputButton,linearLayout_do;
+    private TestData testData=null;
     private OnFragmentInteractionListener mListener;
 
     public EditFragment() {
@@ -51,8 +65,42 @@ public class EditFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_edit, container, false);
+        initView(view);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ("".equals(weight.getText()) || "".equals(value.getText())){
+                    Toast.makeText(getContext(), "重量和价值必须全部填写",Toast.LENGTH_LONG).show();
+                }else {
+                    weights.add(Integer.valueOf(weight.getText().toString()));
+                    values.add(Integer.valueOf(value.getText().toString()));
+                    weight.getText().clear();
+                    value.getText().clear();
+                    Toast.makeText(getContext(), "请录入下一个，结束请按结束按钮",Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+        btn_finshAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //隐藏当前LinearLayout
+                if (weights.size()>0 && values.size()>0){
+                    linearLayout_inputButton.setVisibility(View.GONE);
+                    linearLayout_do.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
         return view;
     }
+
+
+
+
+
+
+
 
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
@@ -80,5 +128,18 @@ public class EditFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void initView(View view){
+         weight= (EditText) view.findViewById(R.id.editText);
+         value= (EditText) view.findViewById(R.id.editText2);
+         btn_add= (Button) view.findViewById(R.id.btn_add);
+         btn_finshAdd= (Button) view.findViewById(R.id.btn_endAdd);
+         btn_inputDo= (Button) view.findViewById(R.id.btn_inputDo);
+         input_list= (ListView) view.findViewById(R.id.list_inputList);
+         sp_input_selectMethod= (Spinner) view.findViewById(R.id.sp_input_selectMethod);
+         input_capacity= (EditText) view.findViewById(R.id.input_capacity);
+        linearLayout_do= (LinearLayout) view.findViewById(R.id.linearLayout_do);
+        linearLayout_inputButton= (LinearLayout) view.findViewById(R.id.linearLayout_inputButton);
     }
 }
